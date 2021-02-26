e4456226f87f04af7350b39035ea4763c25c1747
Bruno Pedrosa <bgpedrosas@gmail.com>
2021-02-26 12:48:12 -0300

    Cria estrutura basica de projeto

---
+++ script.js
@@ -0,0 +1,105 @@
+function onTextareaContentChange(event) {
+  const textarea = event.target;
+  const counter = document.getElementById('textarea-count');
+
+  const newCount = 500 - textarea.value.length;
+  counter.innerText = newCount;
+}
+
+function bindOnChangeToTextareaCounter() {
+  const textarea = document.getElementById('textarea-field');
+  textarea.addEventListener('input', onTextareaContentChange);
+}
+
+function onChangeAgreementCheck(event) {
+  const submitFormBtn = document.getElementById('submit-btn');
+  const isChecked = event.target.checked;
+
+  if (isChecked) {
+    submitFormBtn.disabled = false;
+  } else {
+    submitFormBtn.disabled = true;
+  }
+}
+
+function bindEnableSubmitOnAgreementCheck() {
+  const checkAgreed = document.getElementById('agreement');
+  checkAgreed.addEventListener('change', onChangeAgreementCheck);
+}
+
+function createParagraph(label, content) {
+  const p = document.createElement('p');
+  p.innerText = `${label}: ${content}`;
+  return p;
+}
+
+function onSubmitForm(event) {
+  event.preventDefault();
+
+  const name = document.getElementById('name');
+  const lastName = document.getElementById('lastname');
+  const email = document.getElementById('email');
+  const house = document.getElementById('house');
+
+  const family = Array.from(document.querySelectorAll('.family')).find(radio => radio.checked);
+  const subjects = Array.from(document.querySelectorAll('.subject')).filter(checkbox => checkbox.checked);
+
+  const evaluation = Array.from(document.querySelectorAll('.evaluation')).find(radio => radio.checked);
+  const textArea = document.getElementById('textarea-field');
+
+  event.target.innerHTML = '';
+
+  event.target.appendChild(createParagraph('Nome', name.value));
+  event.target.appendChild(createParagraph('Sobrenome', lastName.value));
+  event.target.appendChild(createParagraph('Email', email.value));
+  event.target.appendChild(createParagraph('Casa', house.value));
+
+  event.target.appendChild(createParagraph('Família', family.value));
+  subjects.forEach(subject => {
+    event.target.appendChild(createParagraph('Matéria', subject.value));
+  });
+
+  event.target.appendChild(createParagraph('Avaliação', evaluation.value));
+  event.target.appendChild(createParagraph('Comentários', textArea.value));
+}
+
+function bindOnSubmitFormEvent() {
+  const form = document.getElementById('evaluation-form');
+  form.addEventListener('submit', onSubmitForm);
+}
+
+function onClickHeaderBtn() {
+  const login = document.getElementById("login-input");
+  const password = document.getElementById("password-input");
+
+  const defaultLogin = "tryber@betrybe.com";
+  const defaultPassword = "123456";
+
+  if (login.value === defaultLogin && password.value === defaultPassword) {
+    alert('Olá Tryber!');
+  } else {
+    alert('Login ou senha inválidos.');
+  }
+
+  login.value = '';
+  password.value = '';
+}
+
+function bindOnClickHeaderBtn() {
+  const loginBtn = document.getElementById('login-btn');
+  loginBtn.addEventListener('click', onClickHeaderBtn)
+}
+
+function execBindEvents() {
+  bindOnChangeToTextareaCounter();
+  bindEnableSubmitOnAgreementCheck();
+  bindOnSubmitFormEvent();
+  bindOnClickHeaderBtn()
+}
+
+window.onload = () => {
+  execBindEvents();
+
+  const submitFormBtn = document.getElementById('submit-btn');
+  submitFormBtn.disabled = true;
+}
